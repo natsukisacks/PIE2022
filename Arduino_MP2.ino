94d367e0ded2c3394219879891a2eebde1a039ef
nsacks2905 <98283053+nsacks2905@users.noreply.github.com>
2022-09-29 08:13:38 -0400

    Update Arduino_MP2.ino

--- Arduino_MP2.ino
+++ Arduino_MP2.ino
@@ -1,52 +1,50 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
-*/
-
 #include <Servo.h>
 
-Servo base_servo;  // create servo object to control a servo
+// Create servo objects
+Servo base_servo;
 Servo sensor_servo;
 
-int pos = 50;    // variable to store the servo position
-int angle = 0;  // bottom starting angle
+// Create variables to store information
+int pos = 50;    // variable to store the base servo position
+int angle = 0;   // variable to store tilt servo position
 const int sensorPin = A0;
 int sensorValue = 0;
+int sum = 0;
+int avgSensor = 0;
+int i = 0;   // for loop incrementer
 
 void setup() {
-  base_servo.attach(9);  // attaches the servo on pin 9 to the servo object
+  base_servo.attach(9);
   sensor_servo.attach(10);
   Serial.begin(9600);
 }
 
-// 105 degrees is "center" for sensor value
-// 100 degrees is "center" for base value
-// return pan, tilt, voltage
 void loop() {
-  sensor_servo.write(105);  // 105- 60
-  base_servo.write(50);
+  sensor_servo.write(110);
+  base_servo.write(70);
 
-  // from side to side
-  for (pos = 50; pos <= 120; pos += 5) {  // 100 degrees is the
-    base_servo.write(pos);               // tell servo to go to position in variable 'pos'
-    // tilt sensor to scan entire length
-    for (angle = 165; angle >= 75; angle -= 1) {
+  // Move the servos!
+  for (angle = 90; angle >= 30; angle -= 1) {
+    for (pos = 50; pos <= 110; pos += 1) {
+      sum = 0;   // Reset the sum variable after each new pan angle
+      base_servo.write(pos);
+      delay(30);
       sensor_servo.write(angle);
-      sensorValue = analogRead(sensorPin);
-      Serial.print(pos); 
-      Serial.print(" "); 
-      Serial.print(angle);
-      Serial.print(" ");
-      Serial.println(sensorValue);
-      delay(15);
-    }
-    delay(30);  // waits 15 ms for the servo to reach the position
-    if (pos == 120) {
-      exit(0);
+
+      // take the average of 30 points at each new angle
+      for (i = 0; i <= 30; i += 1) {
+        sensorValue = analogRead(sensorPin);
+        sum += sensorValue;
+      }
+    avgSensor = sum / i;
+     
+    // Print information to serial monitor as "pan_angle tilt_angle avg_Sensor"
+    Serial.print(pos); 
+    Serial.print(" "); 
+    Serial.print(angle);
+    Serial.print(" ");
+    Serial.print(avgSensor);
+    delay(30);
     }
   }
 }
