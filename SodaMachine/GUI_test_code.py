8d3723e0a7c3ee1d8b35e840c5888ffe4849428b
nsacks2905 <98283053+nsacks2905@users.noreply.github.com>
2022-12-04 14:26:22 -0500

    Update GUI_test_code.py

--- SodaMachine/GUI_test_code.py
+++ SodaMachine/GUI_test_code.py
@@ -1,37 +1,10 @@
-from Tkinter import *
-import tkFont
+from tkinter import *
+import tkinter.font
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(40, GPIO.OUT)
-GPIO.output(40, GPIO.LOW)
-
 win = Tk()
-
-myFont = tkFont.Font(family = 'Helvetica', size = 36, weight = 'bold')
-
-def ledON():
-	print("LED button pressed")
-	if GPIO.input(40) :
- 		GPIO.output(40,GPIO.LOW)
-			ledButton["text"] = "LED ON"
-	else:
-		GPIO.output(40,GPIO.HIGH)
-                	ledButton["text"] = "LED OFF"
-
-def exitProgram():
-	print("Exit Button pressed")
-        GPIO.cleanup()
-	win.quit()	
+win.title("Test")
 
 
-win.title("First GUI")
-win.geometry('800x480')
-
-exitButton  = Button(win, text = "Exit", font = myFont, command = exitProgram, height =2 , width = 6) 
-exitButton.pack(side = BOTTOM)
-
-ledButton = Button(win, text = "LED ON", font = myFont, command = ledON, height = 2, width =8 )
-ledButton.pack()
-
-mainloop()
+myFont = tkinter.font.Font(family = 'Helvetica', size = 36, weight = 'bold')
+button = Button(win, text = "Tester", font = myFont, bg = 'bisque2', height = 1, width = 4)
